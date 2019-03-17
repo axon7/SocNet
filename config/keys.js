756ffa6fd0b1b2ae7fee1e122ee4d0107dd10944
Kuba <kubaz9991@gmail.com>
2019-03-17 21:55:39 +0100

    jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://kubaz9991:dbkubaz9991@ds255754.mlab.com:55754/devconnectorkuba'
+    mongoURI: 'mongodb://kubaz9991:dbkubaz9991@ds255754.mlab.com:55754/devconnectorkuba',
+    secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const User = require("../../models/User");
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const key = require('../../config/keys');
 
 //@route    GET api/users/test
 //@desc     testing users route
@@ -15,6 +18,7 @@ router.get("/test", (req, res) =>{
 //@desc     register user
 //@access   public
 router.post('/register', (req, res)=>{
+    //if the email exists in users collection -> create new user
     User.findOne({email: req.body.email})
        .then(user =>{
            if(user){
@@ -47,5 +51,54 @@ router.post('/register', (req, res)=>{
        })
     
 });
+
+
+//@route    POST api/users/register
+//@desc     login user, returning the token
+//@access   public
+router.post('/login', (req, res)=>{
+    const password = req.body.password;
+    const email = req.body.email;
+
+    User.findOne({email})
+        .then(user =>{
+            //check if the typed email does exist
+            if(!user){
+                return res.status(404).json({msg: "Email not found"});
+            } else{
+                //if exist then check is the password correct
+                bcrypt.compare(password, user.password).then((matching)=>{
+                    if(!matching){
+                        return res.status(400).json({msg: "password incorrect"});
+                    } else {
+                        const payload = {id: user.id, name: user.name, avatar: user.avatar};
+                        jwt.sign(payload, key.secretOrKey,{expiresIn:3600}, (err, token)=>{
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token
+                            })
+                        })
+
+
+                    }
+                })
+
+
+            }
+        })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports=  router;
