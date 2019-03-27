25dded0612e5a3f0ee55255b31b980850674587a
Kuba <kubaz9991@gmail.com>
2019-03-27 21:07:33 +0100

    posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,32 @@
 const express =  require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const passport = require('passport');
+
+//require post model
+const Post = require("../../models/Post");
+
+
+//@route    GET api/profile/test
 router.get("/test", (req, res) =>{
     res.json({msg: "Posts test Ok!"});
 });
 
+
+//@route    GET api/profile/
+//@desc     get current users profile
+//@access   private
+router.post("/", (req, res) =>{
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save.then(post =>{
+        res.json.json(post);
+    })
+
+});
 
 module.exports = router;
