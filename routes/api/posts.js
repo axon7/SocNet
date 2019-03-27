44179490d8e707e1303998d73b3526285e969194
Kuba <kubaz9991@gmail.com>
2019-03-27 20:50:00 +0100

    post schema

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
 const express =  require("express");
 const router = express.Router();
-
+const mongoose = require('mongoose');
 router.get("/test", (req, res) =>{
     res.json({msg: "Posts test Ok!"});
 });
 
 
-module.exports=  router;
+module.exports = router;
