a1ba0915e33c607cc7fc3273de526d90b1998d5b
Kuba <kubaz9991@gmail.com>
2019-04-02 22:05:19 +0200

    basic client-side

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import './App.css';
+
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Landing}/>
+          <Footer/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
