3f15a3aa4f9e04b6365f4e9d48b2b1ba26525570
Kuba <kubaz9991@gmail.com>
2019-04-04 23:15:27 +0200

    react router register login

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
   render() {
@@ -13,6 +15,11 @@ class App extends Component {
         <div className="App">
           <Navbar/>
           <Route exact path="/" component={Landing}/>
+            <div className="container">
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
+
+            </div>
           <Footer/>
         </div>
       </Router>
